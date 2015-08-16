2bdb31cc3eb37a1c345072fccc29446034a5c9a8
mcarlson <matthew.m.carlson@gmail.com>
2015-08-15 17:36:02 -0700

    Added some test cases and made testing more rigorous.

--- tests.py
+++ tests.py
@@ -4,22 +4,35 @@ from rover import Rover_Download_Timer
 
 
 
-tests = [((2000, 15, 10, 7, 
+tests = [
+         ### First example given
+         ((2000, 15, 10, 7, 
            [(0,200), (200,400), (400,600), (600,800), 
             (800,1000), (1000,2000),(0,1800)]), 
-          340.000),
+          '340.000'),
+         ### Second example given
          ((2000, 5, 10, 7, 
            [(0,200), (200,400), (400,600), (600,800), 
             (800,1000), (1000,2000),(0,1800)]), 
-          260.000),
+          '260.000'),
+         ### Negative indexes okay?
+         ((2000, 15, 10, 7, 
+           [(0,200), (200,400), (400,600), (600,800), 
+            (800,1000), (1000,2000),(-10,1790)]), 
+          '340.000'),
+         ### No valid solution
+         ((20000, 15, 10, 7, 
+           [(0,200), (200,400), (400,600), (600,800), 
+            (800,1000), (1000,2000),(-10,1800)]), 
+          ''),
 ]
 
 for input_, expected_result in tests:
     N, L, B, C, chunks = input_
     rover = Rover_Download_Timer(N, L, B, chunks)    
-    res = rover.answer
+    res = rover.answer_str
     if res == expected_result:
         print 'Pass!'
     else:
         print '{res} = Rover_Download_Timer({inp})'.format(res=res, inp=input_)
-        print '(epected {ex})!'.format(expected_result)
+        print '(epected {ex})!'.format(ex = expected_result)
